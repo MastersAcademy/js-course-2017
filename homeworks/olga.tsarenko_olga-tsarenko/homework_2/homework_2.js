f4d14007eb46dc5a587c98d29557e955327ce2d5
olga-tsarenko <olga.tsarenko7@gmail.com>
2017-11-08 23:11:21 +0200

    fix

--- homeworks/olga.tsarenko_olga-tsarenko/homework_2/homework_2.js
+++ homeworks/olga.tsarenko_olga-tsarenko/homework_2/homework_2.js
@@ -1,22 +1,14 @@
 let height = 4;
 if (2 <= height <= 24) {
-
-    let block = "#";
-
-    let space = " ";
+    const block = '#';
+    const space = ' ';
 
     for (let i = 1; i <= height; i++) {
-
-        let rock = block.repeat(i);
-
-        let canyon = space.repeat(2);
-
-        let indentation = space.repeat(height - i);
-
-        let result = indentation + rock + canyon + rock;
-
+        let rock = block.repeat(i),
+            canyon = space.repeat(2),
+            indentation = space.repeat(height - i),
+            result = indentation + rock + canyon + rock;
         console.log(result);
-
     }
 }
-
+console.log('height is', height);
