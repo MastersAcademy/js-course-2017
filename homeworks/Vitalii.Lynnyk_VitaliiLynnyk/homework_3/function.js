1809f52148fb0f5d1b3e222dcd854857d19d3145
VitaliiLynnyk <linnik2101@gmail.com>
2017-11-09 13:20:41 +0200

    homework_3

--- homeworks/Vitalii.Lynnyk_VitaliiLynnyk/homework_3/function.js
+++ homeworks/Vitalii.Lynnyk_VitaliiLynnyk/homework_3/function.js
@@ -1,16 +1,19 @@
-function start(){
-    let leftSide = "";
-    let rightSide = "";
-    let height = parseInt(prompt("Write a height, please = "),10);
-        if (!height) {
-            height = 4 ;
-        }
+let rl = require("readline");
+let prompts = rl.createInterface(process.stdin, process.stdout);
+let leftSide = "";
+let rightSide = "";
+
+prompts.question("Write a height,please = ", function (height) {
+    if (!parseInt(height)){
+        height = 4 ;
+    }
     let counter = height;
-        console.log("Height :",height);
-        for (let i = 0; i < counter; i++) {
-            leftSide = " ".repeat(height-1)+"#".repeat(i+1);
-            rightSide += "#";
-            console.log(leftSide,"",rightSide);
-            height--;
-        }
-}
+    console.log("Height :",height);
+    for (let i = 0; i < counter; i++) {
+        leftSide = " ".repeat(height-1)+"#".repeat(i+1);
+        rightSide += "#";
+        console.log(leftSide,"",rightSide);
+        height--;
+    }
+    process.exit();
+});
