e47d1dffad9b6a6f6baf46c406478e115e975927
ahrrhy <ahrrhy@gmail.com>
2017-11-11 01:22:21 +0200

    hw2 after comments

--- homeworks/stanislav.zhuravel_ahrrhy/homework_2/main.js
+++ homeworks/stanislav.zhuravel_ahrrhy/homework_2/main.js
@@ -1,19 +1,17 @@
 function printPyramid (height) {
-		var str = '',
-			spc = ' ',
-			spcs,
-			pyramidLvl;
-	if (height < 2) {
-		console.log('need at least 2');
-	}else if ( (typeof height) !== 'number') {
-		console.log('need a number');
-	}else {
-		for (let i = 1; i <= height ; i++) {
-			str += '#';
-			spcs = spc.repeat(height-i);
-			pyramidLvl = spcs + str + "  " + str;
-			console.log(pyramidLvl);
-		}
-	}
+    const char = '#',
+        spc = ' ';
+    if (height < 2 && height > 15) {
+        console.log('need at least 2 and less then 15');
+    } else if ( typeof height !== 'number') {
+        console.log('need a number');
+    } else {
+        for (let i = 1; i <= height ; i++) {
+            str = char.repeat(i);
+            let spcs = spc.repeat(height-i),
+                pyramidLvl = spcs + str + '  ' + str;
+            console.log(pyramidLvl);
+        }
+    }
 }
 printPyramid(4);
