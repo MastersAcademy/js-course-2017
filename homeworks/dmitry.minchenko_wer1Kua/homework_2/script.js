423d759420ea47c5b703085b5ae5b577535a5b0a
Dmitry Minchenko <dimaminchenko@gmail.com>
2017-11-11 08:35:21 +0200

    Wrap code in function

--- homeworks/dmitry.minchenko_wer1Kua/homework_2/script.js
+++ homeworks/dmitry.minchenko_wer1Kua/homework_2/script.js
@@ -1,11 +1,13 @@
-let height = 4,
-    block = '#',
-    space = ' ';
+(function() {
+    let height = 1,
+        block = '#',
+        space = ' ';
 
-if (height>=2) {
+    if (height<2) {
+        return console.log('Error! Height must be >= 2');
+    }
+
     for (let i = 0; i < height; i++) {
         console.log(space.repeat(height-i) + block.repeat(i+1) + space.repeat(2) + block.repeat(1+i));
     }
-} else {
-    console.log('Error! Height must be >= 2')
-}
+})();
