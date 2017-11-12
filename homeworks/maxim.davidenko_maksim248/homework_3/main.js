aaf5960f3e8cfe6dbecc2779327b5aff0407d72f
maksim248 <maxscorekd@gmail.com>
2017-11-12 14:56:11 +0200

    homework 3 fix

--- homeworks/maxim.davidenko_maksim248/homework_3/main.js
+++ homeworks/maxim.davidenko_maksim248/homework_3/main.js
@@ -1,8 +1,8 @@
-function build(block, spaceBetween, height) {
-  for (var i = 1; i < height + 1; i++){
-  var buildPyramid = spaceBetween.repeat(height - i) + block.repeat(i) + '  ' + block.repeat(i);
+function build(block, height) {
+  for (let i = 1; i < height + 1; i++){
+  let buildPyramid = ' '.repeat(height - i) + block.repeat(i) + '  ' + block.repeat(i);
   console.log(buildPyramid);
  }
 };
-build('#',' ', 4);
+build('#', 10);
 
