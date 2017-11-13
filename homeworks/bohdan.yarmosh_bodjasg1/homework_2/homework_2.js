c7bd70caf0ff4c5b30acc9ef83cbf12ae6ec032a
bodjaSG1 <bodjaSG1@gmail.com>
2017-11-13 23:06:00 +0200

    3

--- homeworks/bohdan.yarmosh_bodjasg1/homework_2/homework_2.js
+++ homeworks/bohdan.yarmosh_bodjasg1/homework_2/homework_2.js
@@ -1,31 +1,30 @@
-marioProblem(1);
+marioProblem(3);
 
 function marioProblem(height) {
     if( height < 2 ){
         marioProblem(2);
-        } else if( height == undefined ){
-            marioProblem(4);
-            } else {
-                for (var row = 0; row < height; row++) {
-                    var numBricks = row + 1;
-                    var numSpaces = height - row;
-                
-                    var rowStr = "";
-                    for (var i = 0; i < numSpaces; i++) {
-                        rowStr += " ";
-                    }
-                
-                    for (i = 0; i < numBricks; i++) {
-                        rowStr += "#";
-                    }
-                
-                    var rowStr2 = "";
-                    for (i = 0; i < numBricks; i++) {
-                        rowStr2 += "#";
-                    }
-                
-                    console.log(rowStr + "  " + rowStr2);
-                }  
-            }  
+    } else if( height == undefined ){
+        marioProblem(4);
+    } else {
+        for (var row = 0; row < height; row++) {
+            var numBricks = row + 1;
+            var numSpaces = height - row;
+        
+            var rowStr = "";
+            for (var i = 0; i < numSpaces; i++) {
+                rowStr += " ";
+            }
+        
+            for (i = 0; i < numBricks; i++) {
+                rowStr += "#";
+            }
+        
+            var rowStr2 = "";
+            for (i = 0; i < numBricks; i++) {
+                rowStr2 += "#";
+            }
+        
+            console.log(rowStr + "  " + rowStr2);
+        }  
+    }  
 }
-
