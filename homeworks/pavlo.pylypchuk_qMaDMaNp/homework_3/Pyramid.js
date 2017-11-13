901673e4d0b0e4b1807daa1318a5b4055be7e931
Pavlo <medifman@gmail.com>
2017-11-13 22:17:53 +0200

    Update Pyramid.js

--- homeworks/pavlo.pylypchuk_qMaDMaNp/homework_3/Pyramid.js
+++ homeworks/pavlo.pylypchuk_qMaDMaNp/homework_3/Pyramid.js
@@ -6,15 +6,14 @@ const SPACE = " ";
 prompts.question("What`s the height?", function(tower_h) {
 	let height = tower_h;              	
  
-	if(tower_h >= 2) {
-		console.log("Height:"+ height);
-		for(let i = 0; i < height; i++) {
-			console.log(SPACE.repeat(tower_h--) + BLOCK.repeat(i+1) + SPACE.repeat(2) + BLOCK.repeat(i+1));
-		}
+	if(tower_h < 2) {
+	    console.log("Number is not >= 2");
+	process.exit(1);
 	}
-	else {
-	console.log("Number is not >= 2");
+	
+	console.log("Height:"+ height);
+	for(let i = 0; i < height; i++) {
+	    console.log(SPACE.repeat(tower_h--) + BLOCK.repeat(i+1) + SPACE.repeat(2) + BLOCK.repeat(i+1));
 	}
-
-process.exit(1);
+    process.exit(1);		
 });
