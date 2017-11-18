d3dd71edc999d34a2b5ec5f28369fd256e108164
prosst <r.panteleychuk@gmail.com>
2017-11-18 17:31:48 +0200

    homework 3 done

---
+++ homeworks/rostislav.panteleychuk_rpanteleychuk/homework_3/part1.js
@@ -0,0 +1,17 @@
+let arh = ['first', 'second', 'third', 'fourth', 'master'];
+
+let m;
+let a;
+
+arh.forEach(function (item, i, arh) {
+    m = item.match(/m/ig);
+    a = item.match(/a/ig);
+    if (m != null && a != null) {
+        if (m.length == a.length) {
+            console.log(item = 1);
+        }
+    } else {
+        console.log(item = 0);
+    }
+
+});
