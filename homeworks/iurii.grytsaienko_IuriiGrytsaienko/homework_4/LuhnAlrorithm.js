7ce4b816cc7740535186257edf85b1b181b58465
Iurii Grytsaienko <iuriigrytsaenko@gmail.com>
2017-11-18 21:01:01 +0200

    added chek on special characters

--- homeworks/iurii.grytsaienko_IuriiGrytsaienko/homework_4/LuhnAlrorithm.js
+++ homeworks/iurii.grytsaienko_IuriiGrytsaienko/homework_4/LuhnAlrorithm.js
@@ -1,18 +1,21 @@
 function checkLuhn(cardNumber) {
-    cardNumber = String(cardNumber);
-    let sum = 0;
 
-    for (let i = 0; i < cardNumber.length ; i++) {
-        let digit = parseInt(cardNumber[i]);
-        if (i % 2 === 0) {
-            digit *= 2;
-            if (digit > 9) {
-                digit = digit - 9;
+    cardNumber = String(cardNumber);
+    if ((/[^\d\.\,\\\'\"\:\;\-\=\+]/).test(cardNumber)) return null
+    else {
+        let sum = 0;
+        for (let i = 0; i < cardNumber.length; i++) {
+            let digit = parseInt(cardNumber[i]);
+            if (i % 2 === 0) {
+                digit *= 2;
+                if (digit > 9) {
+                    digit = digit - 9;
+                }
             }
+            sum += digit;
         }
-        sum += digit;
+        return Boolean(!(sum % 10))
     }
-    return Boolean(!(sum % 10))
 }
 
-console.log(checkLuhn(44108565956253135));
+console.log(checkLuhn('4410856595625312'));
