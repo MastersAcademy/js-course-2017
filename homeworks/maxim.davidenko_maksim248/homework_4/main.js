ade60122805a84ed7e3fa6b15f75e3882652a374
maksim248 <maxscorekd@gmail.com>
2017-11-20 19:48:54 +0200

    homework_4

---
+++ homeworks/maxim.davidenko_maksim248/homework_4/main.js
@@ -0,0 +1,18 @@
+let someWords = ["first", "Marlin Monroe", "third", "fourth", "Masters academy"];
+let searchLetters = function (someWords) { 
+  return someWords.map(function (letters) {
+    let findM = letters.match(/m/gi),
+        findA = letters.match(/a/gi);
+  if(!findM && !findA) {
+    return 1;
+  } else if(findM.length === findA.length) { 
+    return 1;
+  } else {
+    return 0;
+  }
+})};
+console.log(searchLetters(someWords));
+
+
+
+
