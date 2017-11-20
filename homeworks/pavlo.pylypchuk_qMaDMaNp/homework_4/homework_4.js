535b4df29ff7132f04c54e300f20f4733991259e
qMaDMaNp <medifman@gmail.com>
2017-11-21 01:27:35 +0200

    work_4

---
+++ homeworks/pavlo.pylypchuk_qMaDMaNp/homework_4/homework_4.js
@@ -0,0 +1,32 @@
+let a = ['meomeoi', 'petya', 'masa', 'julik','master academy'];
+let b = a[0].split('');
+let mSaver = [];
+let aSaver = [];
+
+
+(function Savers(chars) {
+	for(let i = 0; i < chars.length; i++) {
+
+		if(b[i] == "m") {
+			mSaver.push("1");
+		}
+
+		if(b[i] == "a") {
+			aSaver.push("1");
+		}
+	}
+})(b);
+
+(function Compare(mArg, aArg) {
+		mLength = mArg.length;
+		aLength = aArg.length;
+
+		if(mLength != aLength){
+			console.log("0");
+		}
+
+		if(mLength == aLength || (mLength == 0 && aLength == 0)){
+			console.log("1");
+		}
+})(mSaver, aSaver);
+
