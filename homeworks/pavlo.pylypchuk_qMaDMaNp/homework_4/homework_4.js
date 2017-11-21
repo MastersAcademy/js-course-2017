9eb848e04efb7eac2b859fb3a916ce28c39254b2
Pavlo <medifman@gmail.com>
2017-11-21 10:50:23 +0200

    Update homework_4.js

--- homeworks/pavlo.pylypchuk_qMaDMaNp/homework_4/homework_4.js
+++ homeworks/pavlo.pylypchuk_qMaDMaNp/homework_4/homework_4.js
@@ -2,8 +2,10 @@ let a = ['meomeoi', 'petya', 'masa', 'julik','master academy'];
 let b = a[0].split(''); // change index here to change the world =)
 let mSaver = [];
 let aSaver = [];
+let answer = [];
 
-
+for(let i = 0; i < a.length; i++) {
+	b = a[i];
 (function Savers(chars) {
 	for(let i = 0; i < chars.length; i++) {
 
@@ -22,11 +24,13 @@ let aSaver = [];
 		aLength = aArg.length;
 
 		if(mLength != aLength){
-			console.log("0");
+			answer.push("0");
 		}
 
 		if(mLength == aLength || (mLength == 0 && aLength == 0)){
-			console.log("1");
+			answer.push("1");
 		}
 })(mSaver, aSaver);
 
+}
+console.log(answer);
