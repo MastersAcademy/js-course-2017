7930629a7499b9ae4e5b25a84c9eaa3da3aef939
Medvedenko Yaroslav <medvedenko96@gmail.com>
2017-11-28 20:55:55 +0200

    fix

--- homeworks/Yaroslav.Medvedenko_medvedenko96/homework_4.2/main.js
+++ homeworks/Yaroslav.Medvedenko_medvedenko96/homework_4.2/main.js
@@ -5,26 +5,26 @@ function Moon(card) {
     if (/[A-Za-z]/g.test(card)) {
         console.log('Error');
         return null;
-    } else {
-        let numberFilter = card.match(/\d/g);
-        for (let i = 0; i < numberFilter.length; i++) {
-            if (i % 2 === 0) {
-                let m = parseInt(numberFilter[i]) * 2;
-                if (m > 9) {
-                    arr.push(m - 9);
-                } else {
-                    arr.push(m);
-                }
-            } else {
-                let n = parseInt(numberFilter[i]);
-                arr.push(n);
-            }
-        }
-        let result = arr.reduce(function (a, b) {
-            return a + b;
-        });
-        return result % 10 === 0;
     }
+    let numberFilter = card.match(/\d/g);
+    for (let i = 0; i < numberFilter.length; i++) {
+        if (i % 2 === 0) {
+            let m = parseInt(numberFilter[i]) * 2;
+            if (m > 9) {
+                arr.push(m - 9);
+            } else {
+                arr.push(m);
+            }
+        } else {
+            let n = parseInt(numberFilter[i]);
+            arr.push(n);
+        }
+    }
+    let result = arr.reduce(function (a, b) {
+        return a + b;
+    });
+    return result % 10 === 0;
+
 }
 console.log(Moon(card));
 
