c9fd9e4980a04bc2dc1a45d5362bf95b71421da3
VadimMir <Miroshnik_vy@ukr.net>
2017-11-30 22:16:53 +0200

    homework4_2

--- homeworks/Vadim.Miroshnik_VadimMir/homework_4/homework4_2.js
+++ homeworks/Vadim.Miroshnik_VadimMir/homework_4/homework4_2.js
@@ -8,19 +8,15 @@ let method_Luna = function (card){
     let sum_of_multiplied = 0;
     let sum = 0;
     let sumCard = 0;
-    let result = 0;
-        
+    let result = 0;      
     let cardValid = function(card){
         if (newCard.search(/\D/) !=-1) return null;
         if (newCard.search(/\d/) !=-1 ) return newCard;        
     };
     
     let pairingCard = function(){
-        if(newCard.length % 2 === 0) {
-            pairing = 0;  
-        } else {
-            pairing = 1;
-        }
+        if(newCard.length % 2 === 0) pairing = 0;  
+        else  pairing = 1;
         return pairing;
     };  
     pairing = pairingCard(pairing);
@@ -36,7 +32,6 @@ let method_Luna = function (card){
         }
         return sum_of_multiplied; 
     };
-
     let lunaCard_sum = function(){
         for (let j = pairing + 1; j < newCard.length; j = j + 2 ){
             let numeral_1 = parseInt(newCard[j]);
@@ -44,14 +39,10 @@ let method_Luna = function (card){
         }   
         return sum;
     };
-
     let lunaCard = function (){
         sumCard =  lunaCard_multiply(sum_of_multiplied) + lunaCard_sum(sum);
-        if (sumCard % 10 === 0) {
-            card = true;
-        } else{
-            card = false;
-        }
+        if (sumCard % 10 === 0)  card = true;
+        else card = false;
         return card;            
     };
     return lunaCard(card);
