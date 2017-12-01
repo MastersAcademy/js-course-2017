66d9499746cf18d7cdadf299c7cdd90465fbe461
VadimMir <Miroshnik_vy@ukr.net>
2017-12-01 10:53:41 +0200

    homework4_2

--- homeworks/Vadim.Miroshnik_VadimMir/homework_4/homework4_2.js
+++ homeworks/Vadim.Miroshnik_VadimMir/homework_4/homework4_2.js
@@ -1,6 +1,6 @@
 let card = '4561 2612 1234 5464';
 let method_Luna = function (card){        
-    newCard = card.replace(/\s|-/g, '');
+    let newCard = card.replace(/\s|-/g, '');
     let pairing;
     let multiply = 0; 
     let sum_of_multiplied = 0;
