24ac255d90b5debeedeb01ab86d6850a7e150678
VadimMir <Miroshnik_vy@ukr.net>
2017-12-01 11:18:58 +0200

    homework4_2

--- homeworks/Vadim.Miroshnik_VadimMir/homework_4/homework4_2.js
+++ homeworks/Vadim.Miroshnik_VadimMir/homework_4/homework4_2.js
@@ -1,24 +1,21 @@
 let card = '4561 2612 1234 5464';
-let method_Luna = function (card){        
+let methodLuna = function (card){        
     let newCard = card.replace(/\s|-/g, '');
-    let pairing;
-    let multiply = 0; 
     let sum_of_multiplied = 0;
     let sum = 0;
-    let sumCard = 0;
     let result = 0;      
     let cardValid = function(card){
         if (newCard.search(/\D/) !=-1) return null;
         if (newCard.search(/\d/) !=-1 ) return newCard;        
     }; 
-    let pairingCard = function(){
-        if(newCard.length % 2 === 0) return newCard.length % 2;  
+    let pairingCard = function(card){
+        return newCard.length % 2;  
     };  
-    pairing = pairingCard();
-    let lunaCard_multiply = function(){     
+    let pairing = pairingCard();
+    let lunaCardMultiply = function(card){     
         for(let i = pairing; i < newCard.length; i = i + 2){
             let numeral = parseInt(newCard[i]);
-            multiply = numeral * 2; 
+            let multiply = numeral * 2; 
             if (multiply > 9){
                 multiply = multiply - 9;
             }
@@ -26,19 +23,17 @@ let method_Luna = function (card){
         }
         return sum_of_multiplied; 
     };
-    let lunaCard_sum = function(){
+    let lunaCardSum = function(card){
         for (let j = pairing + 1; j < newCard.length; j = j + 2 ){
             let numeral_1 = parseInt(newCard[j]);
             sum += numeral_1;
         }   
         return sum;
     };
-    let lunaCard = function (){
-        sumCard =  lunaCard_multiply(sum_of_multiplied) + lunaCard_sum(sum);
+    let lunaCard = function (card){
+        let sumCard =  lunaCardMultiply(sum_of_multiplied) + lunaCardSum(sum);
         return sumCard % 10 === 0;                
     };
     return lunaCard(card);
 };  
-console.log(method_Luna(card));
-
-
+console.log(methodLuna(card));
