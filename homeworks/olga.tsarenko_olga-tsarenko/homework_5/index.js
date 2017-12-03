618def48c077e441be4ee5ee1e52529ee310a5e7
olga-tsarenko <olga.tsarenko7@gmail.com>
2017-12-03 12:42:56 +0200

    all levels

--- homeworks/olga.tsarenko_olga-tsarenko/homework_5/index.js
+++ homeworks/olga.tsarenko_olga-tsarenko/homework_5/index.js
@@ -1,15 +1,57 @@
-const startTime = Date.now();
+const START = Date.now();
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 let second = 1000;
 let minute = second * 60;
 let milSec = second / 1000;
+let isON = false;
+let store;
+let timestampsArr = [];
 
-setInterval(
-    ()=>{
-        const currentTime = Date.now();
-        const diff = Number(currentTime - startTime);
-        let minutes = Math.floor((diff % (1000 * 60 * 60)) / minute);
-        let seconds = Math.floor(diff % minute / second);
-        let milSeconds = Math.floor(diff % second / milSec);
-        console.log('\033c');
-        console.log ( 'timer ' + minutes + ':' + seconds + ':'  + milSeconds);
-    }, 10);
+readline.emitKeypressEvents(process.stdin);
+
+process.stdin.setRawMode(true);
+
+function myTimer() {
+    setInterval(
+        () => {
+            const currentTime = Date.now();
+            const diff = Number(currentTime - START);
+            let minutes = Math.floor((diff % (1000 * 60 * 60)) / minute);
+            let seconds = Math.floor(diff % minute / second);
+            let milSeconds = Math.floor(diff % second / milSec);
+            let result = minutes + ':' + seconds + ':' + milSeconds;
+            store = result;
+            rl.output.write('\033[2J\033[0f');
+            rl.write('To start timer press \'s\'. \n');
+            rl.write('To remember current value press \'Space\'. \n');
+            rl.write('To quit timer press \'f\'. \n');
+            rl.write(result + '\r');
+            if (timestampsArr.length) {
+                timestampsArr.forEach((item, index) => rl.write(index + 1 + '.' + ' ' + item + '\r'));
+            }
+        }, 100);
+}
+
+rl.input.on('keypress', (str, key) => {
+    if (key.name === 's' && isON === false) {
+        isON = true;
+
+        myTimer();
+
+    } else if (key.name === 'space' && isON === true) {
+        timestampsArr.push(store);
+    } else if (key.name === 'f' && isON === true) {
+        isON = false;
+        rl.output.write('\033[2K\n');
+        process.exit();
+    }
+});
+
+rl.write('To start timer press \'s\'. \n');
+rl.write('To remember current value press \'Space\'. \n');
+rl.write('To quit timer press \'f\'. \n');
