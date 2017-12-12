84d150321bfebe02502bb58ac0696bd83f24c92c
phantom1723 <andruhin2@ukr.net>
2017-12-12 17:12:27 +0200

    add Homework_6

--- homeworks/evgenia.andrukhina_phantom1723/homework_6/6_1.js
+++ homeworks/evgenia.andrukhina_phantom1723/homework_6/6_1.js
@@ -1,16 +1,28 @@
-let spy = 0;
-
 function freezeFactory(number) {
-    spy++;
-    if (spy % number === 0) {
-        console.log(number);
-    }
+    let spy = 1;
+
+    return function () {
+
+        if (spy % number === 0) {
+            console.log(number);
+
+        } else console.log('---');
+
+        spy++;
+    };
 }
 
-function freeze(number) {
+let frozen = freezeFactory(2);
+let frozen1 = freezeFactory(5);
+
+function multiplyCall(func) {
     for (let i = 1; i <= 10; i++) {
-        freezeFactory(number);
+        func();
     }
 }
 
-freeze(5);
+multiplyCall(frozen1);
+
+
+
+
