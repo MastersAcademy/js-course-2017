89d82be9f54d4f1467582202e8bc93c774b1efc1
apolon23 <polybotko23@gmail.com>
2018-01-04 12:15:54 +0200

    hw5_1

--- homeworks/vyacheslav.polybotko_Apolon23/homework_4/homework4_1.js
+++ homeworks/vyacheslav.polybotko_Apolon23/homework_4/homework4_1.js
@@ -1,20 +1,17 @@
-let myArray = ["first", "second", "third", "fourth", "masters academy", "mma", "ma"]
-let questA = 'a'
-let questM = 'm'
-let searchSymbol = myArray.map(function(myArray) {
-    let resultArray = 0;
+const myArray = ["first", "second", "third", "fourth", "masters academy", "mma", "ma"]
+const questA = 'a'
+const questM = 'm'
+let searchSymbol = myArray.map((myArray) => {
     let considerA = 0;
     let considerM = 0;
     for (let i = 0; i < myArray.length; i++) {
         if (myArray[i] === questA) considerA++;
         if (myArray[i] === questM) considerM++;
     }
-    considerA !== considerM ? resultArray = 0 : resultArray = 1;
-    return resultArray
+    return considerA !== considerM ? 0 : 1;
 });
+
 console.log(searchSymbol);
-
-
 
 
 
