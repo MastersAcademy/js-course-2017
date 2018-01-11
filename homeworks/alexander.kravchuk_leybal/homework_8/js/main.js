246e9f2e743dcfa58d07d4a4df4a09fd09b7a50e
leybal <leybal@ukr.net>
2018-01-11 11:27:09 +0200

    hw 8

---
+++ homeworks/alexander.kravchuk_leybal/homework_8/js/main.js
@@ -0,0 +1,62 @@
+const spinner = document.querySelector('.spinner')
+let items = document.querySelectorAll(".item");
+let audios = [];
+
+
+function playAudio(el) {
+    let audio = new Audio(),
+        title = el.getAttribute("data-audio")
+        src = "audio/" + title + ".mp3";
+
+    audio.src = src;
+    audio.play();
+    audio.loop = false;
+
+    items.forEach(function(item) {
+        return item.classList.remove("active");
+    });
+    el.classList.add("active");
+    if (!spinner.classList.contains('visible')) {
+        spinner.classList.add("visible")
+    }
+
+    audios.push(audio);
+}
+
+
+function mouseEvent(e) {
+     return playAudio(this);
+}
+
+items.forEach(function (el) {
+    el.addEventListener("mousedown", mouseEvent, false);
+});
+
+
+function keyEvent(e) {
+    switch(e.keyCode) {
+        case 81: playAudio(items[0]); break;
+        case 87: playAudio(items[1]); break;
+        case 69: playAudio(items[2]); break;
+
+        case 65: playAudio(items[3]); break;
+        case 83: playAudio(items[4]); break;
+        case 68: playAudio(items[5]); break;
+
+        case 90: playAudio(items[6]); break;
+        case 88: playAudio(items[7]); break;
+        case 67: playAudio(items[8]); break;
+
+        case 32: stopAudio(); break;
+    }
+}
+
+window.addEventListener("keydown", keyEvent, false);
+
+
+function stopAudio() {
+    audios.forEach(audio =>audio.pause());
+    spinner.classList.remove("visible");
+    items.forEach(item => item.classList.remove("active"));
+}
+document.querySelector('.stop').addEventListener("click", stopAudio, false); 
