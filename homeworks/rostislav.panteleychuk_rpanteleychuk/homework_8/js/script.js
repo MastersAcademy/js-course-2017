7e32de801464db5e2510a536e0d9d44bca20aa62
prosst <r.panteleychuk@gmail.com>
2018-01-13 13:10:34 +0200

    sound click done

---
+++ homeworks/rostislav.panteleychuk_rpanteleychuk/homework_8/js/script.js
@@ -0,0 +1,45 @@
+function soundClickQ() {
+    let audio = new Audio();
+    audio.src = 'sounds/boom.wav';
+    audio.autoplay = true;
+}
+function soundClickW() {
+    let audio = new Audio();
+    audio.src = 'sounds/clap.wav';
+    audio.autoplay = true;
+}
+function soundClickE() {
+    let audio = new Audio();
+    audio.src = 'sounds/hihat.wav';
+    audio.autoplay = true;
+}
+function soundClickR() {
+    let audio = new Audio();
+    audio.src = 'sounds/kick.wav';
+    audio.autoplay = true;
+}
+function soundClickT() {
+    let audio = new Audio();
+    audio.src = 'sounds/openhat.wav';
+    audio.autoplay = true;
+}
+function soundClickY() {
+    let audio = new Audio();
+    audio.src = 'sounds/ride.wav';
+    audio.autoplay = true;
+}
+function soundClickU() {
+    let audio = new Audio();
+    audio.src = 'sounds/snare.wav';
+    audio.autoplay = true;
+}
+function soundClickI() {
+    let audio = new Audio();
+    audio.src = 'sounds/tink.wav';
+    audio.autoplay = true;
+}
+function soundClickO() {
+    let audio = new Audio();
+    audio.src = 'sounds/tom.wav';
+    audio.autoplay = true;
+}
