909b92b97ef3ae01cf75d924bb7f522f6b121568
prosst <r.panteleychuk@gmail.com>
2018-01-15 21:39:44 +0200

    key press done

--- homeworks/rostislav.panteleychuk_rpanteleychuk/homework_8/js/script.js
+++ homeworks/rostislav.panteleychuk_rpanteleychuk/homework_8/js/script.js
@@ -1,3 +1,35 @@
+//key press
+function playSound(url){
+    let s = new Audio();
+    s.src = url;
+    s.play();
+}
+
+$(document).keypress(function (e) {
+    if (e.which == 81 || e.which == 113)
+        playSound('sounds/boom.wav');
+    if (e.which == 87 || e.which == 119)
+        playSound('sounds/clap.wav');
+    if (e.which == 69 || e.which == 101)
+        playSound('sounds/hihat.wav');
+    if (e.which == 82 || e.which == 114)
+        playSound('sounds/kick.wav');
+    if (e.which == 84 || e.which == 116)
+        playSound('sounds/openhat.wav');
+    if (e.which == 89 || e.which == 121)
+        playSound('sounds/ride.wav');
+    if (e.which == 85 || e.which == 117)
+        playSound('sounds/snare.wav');
+    if (e.which == 73 || e.which == 105)
+        playSound('sounds/tink.wav');
+    if (e.which == 79 || e.which == 111)
+        playSound('sounds/tom.wav');
+})
+
+
+
+
+//mouse click
 function soundClickQ() {
     let audio = new Audio();
     audio.src = 'sounds/boom.wav';
