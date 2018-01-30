237f76b3adb65d32d48d922415c62530abf154a6
im <todorishina.marina@mail.ru>
2018-01-30 23:38:15 +0200

    Add array of tasks

---
+++ homeworks/marina.todorishina_marinaTodorishina/homework_9/ng-todo-list/src/app/todo/tasks-list.ts
@@ -0,0 +1,7 @@
+import {Task} from "./task";
+
+export const Tasks: Task[] = [
+  {id: 1, name: 'Ride on a bike',status: 'OPEN'},
+  {id: 2, name: 'Programming',status: 'OPEN'},
+  {id: 3, name: 'Draw',status: 'OPEN'},
+]
