27d7eba54fe99534c12c44315ab0305fc7b0ae7c
BaltazoR <baltazorich@gmail.com>
2018-02-05 04:39:01 +0200

    nodejs_homework_3

---
+++ homeworks/maksym.vetrov_BaltazoR/nodejs_homework_3/migrations/20180205021219-create-user.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      bornCountry: {
+        type: Sequelize.STRING
+      },
+      info: {
+        type: Sequelize.STRING
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
